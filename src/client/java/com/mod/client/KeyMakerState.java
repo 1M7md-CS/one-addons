e2b80f4fffc8c86e32909d8783a0fef8a2fb4aa8
1M7md-CS <1M7md-CS@users.noreply.github.com>
2026-06-25 14:39:31 +0300

    Add KeyMakerState enum

---
+++ src/client/java/com/mod/client/KeyMakerState.java
@@ -0,0 +1,12 @@
+package com.mod.client;
+
+public enum KeyMakerState {
+    IDLE,
+    SCANNING,
+    COLLECTING,
+    CRAFT_FURNACE,
+    WAIT_PROCESS,
+    CONFIRM,
+    CLOSE_ABORT,
+    WAIT_FORGE_RETURN
+}
